5ec29db97e77a45b646b732ad9006aa7fc502767
Thapelo <benzitto@gmail.com>
2021-08-22 16:48:31 +0200

    upcoming events component

---
+++ src/components/Upcoming.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Upcoming() {
+    return (
+        <div className="">
+            <h1>Upcoming Component</h1>
+        </div>)
+}
+
+export default Upcoming;
