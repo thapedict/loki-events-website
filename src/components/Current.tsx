599c0af5f546d3529f5d3826a5a2ee2a4a402d35
Thapelo <benzitto@gmail.com>
2021-09-11 14:57:08 +0200

    feat: waiting for api/redux then showing events

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -1,44 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { eventsSelector, fetchEvents } from '../redux/eventsSlice';
+import { useAppDispatch } from '../redux/hooks';
+import GMap from "./GMap";
 
 function Current() {
+    const { events, loading, hasErrors } = useSelector(eventsSelector);
+    const dispatch = useAppDispatch();
+
+    useEffect( () => {
+        dispatch(fetchEvents())
+    }, [dispatch] )
+
+    if(loading) {
+        return <p>
+            Loading... Please Wait!
+        </p>
+    }
+
+    if(hasErrors) {
+        return <p>
+            An Error occured!
+        </p>
+    }
+
     return (
         <section id="current" className="">
             <div className="container">
                 <h2 id="section-title">Current Events</h2>
                 <div id="current-wrap" className="ts-dl-1-3">
                     <div className="event-loop">
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
-                        <div>
-                            <span id="title">Trade Convention</span>
-                            <span id="address">22 Mary Street, Braamfontien, Johannesnurg, 0210</span>
-                        </div>
+                        {
+                            events.map(event => {
+                                return (<div key={event.eventID}>
+                                    <h5 className="title">{event.title}</h5>
+                                    <span className="address">{event.address.street}</span>
+                                </div>)
+                            })
+                        }                        
                     </div>
-                    <div id="map-wrap">
-                        <img src="img/map.jpg" />
-                    </div>
+                    <GMap />
                 </div>
             </div>
         </section>)
